e6ac0e2e5b4a768192e5e67bc76e0698c62c89cd
Andrew Hagedorn <andrewhagedorn@Andrews-MBP.home>
2016-05-22 21:40:38 -0400

    pull instead of building

--- deploy/init-dynamo.sh
+++ deploy/init-dynamo.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-IMAGE_NAME=$1
-
-echo $IMAGE_NAME
-
-cd docker
-docker build -t $IMAGE_NAME .
-cd ..
-
-ID=$(docker run -d -p 8000:8000 $IMAGE_NAME -sharedDb)
+docker pull chagedorn/initialize-local-dynamo
+ID=$(docker run -d -p 8000:8000 chagedorn/initialize-local-dynamo -sharedDb)
 echo $ID
