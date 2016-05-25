dbe0312ab5a8fff0a864ddb960fcd3b2f093a2ae
Andrew Hagedorn <andrewhagedorn@Andrews-MBP.home>
2016-05-24 20:24:14 -0400

    don't run this in travis

--- integration-tests/saver.js
+++ integration-tests/saver.js
@@ -1,15 +1,10 @@
 var index = require('../dist/index');
 var process = require('process');
 
-var username = process.env.DOCKER_USER;
-var password = process.env.DOCKER_PASSWORD;
 var repository = "chagedorn/initialize-local-dynamo";
 var tag = "test";
 
 index.SaveChanges(repository, tag)
-.then(function() {
-    return index.Push(username, password, repository, tag)
-})
 .catch(function(err) {
     console.log(err);
     throw Error(err);
